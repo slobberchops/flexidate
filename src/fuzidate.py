b190c5e584c9641e54889154880677c190799246
Rafe Kaplan <slobberchops@gmail.com>
2019-03-10 00:26:04 -0800

    Implemented converting date objects to fuzidates

--- src/fuzidate.py
+++ src/fuzidate.py
@@ -11,6 +11,21 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import datetime
+
+
+def date_to_number(date: datetime.date):
+    """
+    Convert date to fuzidate number representation.
+
+    Args:
+        date: A date.
+
+    Returns:
+        Number Representation of date as an int number. For example,
+        Nov 11th 1918 is represented as 19181111.
+    """
+    return date.day + date.month * 100 + date.year * 10000
 
 
 class Fuzidate:
@@ -22,7 +37,16 @@ class Fuzidate:
     def __init__(self, number: int):
         self.__number = number
 
+    @classmethod
+    def from_date(cls, date: datetime.date):
+        return cls(date_to_number(date))
+
+
+Fuzidate.max = Fuzidate.from_date(datetime.date.max)
+Fuzidate.min = Fuzidate.from_date(datetime.date.min)
+
 
 __all__ = [
+    date_to_number.__name__,
     Fuzidate.__name__,
 ]
--- test/test_fuzidate.py
+++ test/test_fuzidate.py
@@ -11,13 +11,22 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import datetime
 
 import fuzidate
 
+ARMISTICE = datetime.date(1918, 11, 11)
+ARMISTICE_FZD = fuzidate.Fuzidate(19181111)
 
-class TestAttributes:
 
-    @staticmethod
-    def test_number():
-        number = fuzidate.Fuzidate(19181111).number
-        assert number == 19181111
+def test_number():
+    number = fuzidate.Fuzidate(19181111).number
+    assert number == 19181111
+
+
+def test_date_to_number():
+    assert fuzidate.date_to_number(ARMISTICE) == 19181111
+
+
+def test_from_date():
+    assert fuzidate.Fuzidate.from_date(ARMISTICE).number == 19181111
